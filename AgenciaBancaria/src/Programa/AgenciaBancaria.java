8511036e479ab47dcab47e802b3fc68fbad1f4f8
BrunoApMarques <brunoa879@gmail.com>
2022-09-19 23:23:04 -0300

    correction line 8 and change line 59

--- AgenciaBancaria/src/Programa/AgenciaBancaria.java
+++ AgenciaBancaria/src/Programa/AgenciaBancaria.java
@@ -5,62 +5,62 @@ import java.util.Scanner;
 
 public class AgenciaBancaria {
 
-	static Scanner sc = new Scanner(System.in);
+	static Scanner input = new Scanner(System.in);
 	static ArrayList<Conta> contasBancarias;
-	
+
 	public static void main(String[] args) {
 		contasBancarias = new ArrayList<Conta>();
-		Operacoes();
+		operacoes();
 	}
-	
+
 	public static void operacoes() {
-		
-		 System.out.println("------------------------------------------------------");
-	        System.out.println("-------------Bem vindos a nossa Agência---------------");
-	        System.out.println("------------------------------------------------------");
-	        System.out.println("***** Selecione uma operação que deseja realizar *****");
-	        System.out.println("------------------------------------------------------");
-	        System.out.println("|   Opção 1 - Criar conta   |");
-	        System.out.println("|   Opção 2 - Depositar     |");
-	        System.out.println("|   Opção 3 - Sacar         |");
-	        System.out.println("|   Opção 4 - Transferir    |");
-	        System.out.println("|   Opção 5 - Listar        |");
-	        System.out.println("|   Opção 6 - Sair          |");
-	        
-	        
-	        int operacao = input.nextInt();;
 
-	        switch (operacao) {
-	            case 1:
-	                criarConta();
-	                break;
+		System.out.println("------------------------------------------------------");
+		System.out.println("-------------Bem vindos a nossa Agência---------------");
+		System.out.println("------------------------------------------------------");
+		System.out.println("***** Selecione uma operação que deseja realizar *****");
+		System.out.println("------------------------------------------------------");
+		System.out.println("|   Opção 1 - Criar conta   |");
+		System.out.println("|   Opção 2 - Depositar     |");
+		System.out.println("|   Opção 3 - Sacar         |");
+		System.out.println("|   Opção 4 - Transferir    |");
+		System.out.println("|   Opção 5 - Listar        |");
+		System.out.println("|   Opção 6 - Sair          |");
 
-	            case 2:
-	                depositar();
-	                break;
+		int operacao = input.nextInt();
+		;
 
-	            case 3:
-	                sacar();
-	                break;
+		switch (operacao) {
+		case 1:
+			criarConta();
+			break;
 
-	            case 4:
-	                transferir();
-	                break;
+		case 2:
+			depositar();
+			break;
 
-	            case 5:
-	                listarContas();
-	                break;
+		case 3:
+			sacar();
+			break;
 
-	            case 6:
-	                System.out.println("Flw é nóis!");
-	                System.exit(0); // para o sistema
+		case 4:
+			transferir();
+			break;
 
-	            default:
-	                System.out.println("Opção inválida!");
-	                operacoes();
-	                break;
-	                
-	        }
+		case 5:
+			listarContas();
+			break;
+
+		case 6:
+			System.out.println("Flw é nóis, Thank you for using the BruBank agency");
+			System.exit(0); // para o sistema
+
+		default:
+			System.out.println("Opção inválida!");
+			operacoes();
+			break;
+
+		}
 	}
-	
+
 }
